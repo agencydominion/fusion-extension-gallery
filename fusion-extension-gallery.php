d6807aefafa8f1b11cfa2ba7b01bfe1fb3175198
Jeff Chew <jeff.chew@agencydominion.com>
2016-09-26 14:06:13 -0300

    Update plugin URL

--- fusion-extension-gallery.php
+++ fusion-extension-gallery.php
@@ -5,7 +5,7 @@
  
 /**
  * Plugin Name: Fusion : Extension - Gallery
- * Plugin URI: http://fusion.1867dev.com/
+ * Plugin URI: http://www.agencydominion.com/fusion//
  * Description: Gallery Extension Package for Fusion.
  * Version: 1.1.0
  * Author: Agency Dominion
