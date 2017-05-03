c03ba7bad8f2cd4127e64d6af173b88c2fe6e1bf
Stephen Smith <stephen.smith@agencydominion.com>
2017-05-03 16:31:18 -0400

    Updating version number

--- fusion-extension-gallery.php
+++ fusion-extension-gallery.php
@@ -7,7 +7,7 @@
  * Plugin Name: Fusion : Extension - Gallery
  * Plugin URI: http://www.agencydominion.com/fusion/
  * Description: Gallery Extension Package for Fusion.
- * Version: 1.2.3
+ * Version: 1.2.4
  * Author: Agency Dominion
  * Author URI: http://agencydominion.com
  * License: GPL2
